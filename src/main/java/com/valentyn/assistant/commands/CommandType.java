f591b815fa1874b5d450ee5e5fb6744c54f27c45
Valentyn_Koval <J.K.play2015@gmail.com>
2026-03-16 21:29:08 +0200

    create command type enum

---
+++ src/main/java/com/valentyn/assistant/commands/CommandType.java
@@ -0,0 +1,11 @@
+package com.valentyn.assistant.commands;
+
+public enum CommandType {
+    OPEN_BROWSER,
+    OPEN_TELEGRAM,
+    OPEN_VSCODE,
+    OPEN_CALCULATOR,
+    OPEN_YOUTUBE,
+    EXIT,
+    UNKNOWN
+}
